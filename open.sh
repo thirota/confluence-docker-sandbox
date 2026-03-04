ea071b5c3a19bc007ee2323f1712b7ebd950d326
Takayuki Hirota <2185938+thirota@users.noreply.github.com>
2026-03-04 19:31:09 +0900

    feat: Jump in Confluence DC licenses directly

--- open.sh
+++ open.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 open http://localhost:8090
-open https://developer.atlassian.com/platform/marketplace/timebomb-licenses-for-testing-server-apps/
+open https://developer.atlassian.com/platform/marketplace/timebomb-licenses-for-testing-server-apps/#data-center-host-product-licenses
